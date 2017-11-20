4fff9a0ca8349e82550c170561f906f9a7565b82
Anthony <moi@ici.com>
2017-11-20 08:44:38 -0500

    Initial Commit

---
+++ jouerUneVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jouerUneVideo
+//
+//  Created by Alain on 17-11-20.
+//  Copyright © 2017 Alain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
